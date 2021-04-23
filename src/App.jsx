089c83e07c770cbb4d630671c613fa73ca066f2c
MirrorIce <12020876+MirrorIce@users.noreply.github.com>
2021-04-24 00:18:53 +0300

    Add private route for protecting unwanted routing

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,39 @@ import MainPage from './components/MainPage.jsx';
 import Login from './components/Login.jsx';
 import Registration from './components/Registration.jsx';
 
+function isLogged()
+{
+  fetch('/isLoggedIn',{
+    method:'GET',
+    headers: {
+      'Accept': 'application/json',
+      'Content-Type': 'application/json',
+  }
+  }).then(function(response){
+    if (response.status == 200)
+    {
+      return true;
+    }
+    else
+    {
+      return false;
+    }
+  });
+}
+/*destructure the props properties into two parts
+1) find the component property and assign it to Component so that it can be interpreted correctly 
+2) take the rest of the properties and use them
+*/
+function PrivateRoute({component: Component, ...rest})
+{
+  let logged = isLogged();
+  return(
+    <Route
+     {...rest}
+     render = { (props) => logged?<Component {...props} />:<Redirect to='/login' /> } /> 
+  )
+}
+
 class App extends Component {
   constructor(props) {
     super(props);
@@ -12,14 +45,12 @@ class App extends Component {
 
   render() {
     return (
-      <div>
       <Switch>
-        <Route exact path = '/' />
         <Route exact path = '/login'  component = {Login}  />
         <Route exact path = '/create' component = {Registration} />
         <Route exact path = '/home' component={MainPage}    />
+        <PrivateRoute exact path = '/' component = {MainPage}></PrivateRoute>
       </Switch>
-      </div>
     );
   }
 }
